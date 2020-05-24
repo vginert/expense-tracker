d3ef94132ecd39d264b1cbae9d1f424b86e0252d
Vicente Giner <vginert@gmail.com>
2020-05-24 20:35:14 +0200

    Implement navigation to create transaction screen

--- app/src/main/java/com/vginert/expensetracker/presentation/features/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/vginert/expensetracker/presentation/features/dashboard/DashboardFragment.kt
@@ -1,11 +1,31 @@
 package com.vginert.expensetracker.presentation.features.dashboard
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.vginert.expensetracker.R
 import com.vginert.expensetracker.presentation.core.fragments.BaseFragment
+import com.vginert.expensetracker.presentation.core.lifecycle.observeEvent
+import kotlinx.android.synthetic.main.dashboard_fragment.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class DashboardFragment : BaseFragment() {
 
     override val viewModel: DashboardViewModel by viewModel()
     override val layout: Int = R.layout.dashboard_fragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        observeEvent(viewModel.goToCreateTransaction) { navigateToCreateTransaction() }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        createTransactionButton.setOnClickListener { viewModel.onCreateTransactionClick() }
+    }
+
+    private fun navigateToCreateTransaction() {
+        val action = DashboardFragmentDirections.actionDashboardFragmentToTransactionFragment()
+        findNavController().navigate(action)
+    }
 }
--- app/src/main/java/com/vginert/expensetracker/presentation/features/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/vginert/expensetracker/presentation/features/dashboard/DashboardViewModel.kt
@@ -1,6 +1,17 @@
 package com.vginert.expensetracker.presentation.features.dashboard
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.vginert.expensetracker.domain.core.exceptions.ExceptionHandler
 import com.vginert.expensetracker.presentation.core.fragments.BaseViewModel
+import com.vginert.expensetracker.presentation.core.lifecycle.Event
 
-class DashboardViewModel(exceptionHandler: ExceptionHandler) : BaseViewModel(exceptionHandler)
+class DashboardViewModel(exceptionHandler: ExceptionHandler) : BaseViewModel(exceptionHandler) {
+
+    private val _goToCreateTransaction: MutableLiveData<Event<Unit>> = MutableLiveData()
+    val goToCreateTransaction: LiveData<Event<Unit>> = _goToCreateTransaction
+
+    fun onCreateTransactionClick() {
+        _goToCreateTransaction.value = Event(Unit)
+    }
+}
